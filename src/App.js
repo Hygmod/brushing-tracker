014692d2170666ccb94d602720bb5e969363d6ae
Hygmod <joshsdanton@gmail.com>
2022-09-15 22:58:10 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState, useEffect } from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import StartButton from "./components/StartButton"
+import ProgressBar from "./components/ProgressBar"
 
 function App() {
+  const [complete, setComplete] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => {setComplete(complete => complete + 1)}, 1000);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StartButton />
+      <ProgressBar complete={complete} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/StartButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const StartButton = () => {
+  return (
+    <div>
+        <p>Hello World</p>
+        <button>
+            Start
+        </button>
+    </div>
+  )
+}
+
+export default StartButton
