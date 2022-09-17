333e5fff062eb30a788686b2cb3943afcc140f20
Hygmod <joshsdanton@gmail.com>
2022-09-16 18:48:24 -0700

    updated app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React,{ useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import logo from "./logo.svg"
 import "./App.css"
 import StartButton from "./components/StartButton"
 import ProgressBar from "./components/ProgressBar"
 
 function App() {
-  const [complete, setComplete] = useState(0);
+  const [complete, setComplete] = useState(0)
+  const [isActive, setIsActive] = useState(false)
+
+  function toggle() {
+    console.log("click")
+    setIsActive(!isActive)
+  }
 
   useEffect(() => {
-    setInterval(() => {setComplete(complete => complete + 1)}, 1000);
-  }, []);
+    let interval = null
+    if (isActive) {
+      interval = setInterval(() => {
+        setComplete((complete) => complete + 1)
+      }, 1000)
+    } else if (!isActive && complete !== 0) {
+      clearInterval(interval)
+    }
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <div className="App">
-      <StartButton />
-      <ProgressBar complete={complete} />
+      <StartButton isActive={isActive} onClick={toggle} />
+      <ProgressBar bgcolor="#fcba03" complete={complete} />
     </div>
   )
 }
